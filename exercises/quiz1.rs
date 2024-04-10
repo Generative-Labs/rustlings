63acf12ff961dd3e6296558bfb69938c472d72b2
Binx <zjxubinbin@qq.com>
2024-04-10 07:45:07 +0000

    Finished exercises 08

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,11 +13,14 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
-
+fn calculate_price_of_apples(apples_count: u16) -> u16 {
+    if apples_count > 40 {
+        apples_count
+    } else {
+        apples_count * 2
+    }
+}
 // Don't modify this function!
 #[test]
 fn verify_test() {
